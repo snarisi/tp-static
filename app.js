a008fbdb67c70b70a4c39614ec699820f8fff45a
Gabriel Lebec <glebec@gmail.com>
2015-11-24 10:24:30 -0500

    mob programming ended

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express'),
+    swig = require('swig'),
+    bodyParser = require('body-parser'),
+    morgan = require('morgan'),
+    routes = require('./routes');
+
+var app = express();
+
+app.use(morgan('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(3000, function(err){
+  console.log('Listening to port 3000');
+});
